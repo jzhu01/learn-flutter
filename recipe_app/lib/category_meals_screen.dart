d3412d0bec8f482c0c1862af71ad11499c7fe364
Jen Zhu <jennifer0zhu@gmail.com>
2022-03-03 20:20:42 -0500

    first navigation on app

---
+++ recipe_app/lib/category_meals_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('The Recipes'),
+      ),
+      body: Center(
+        child: Text("Recipes for the Category Go here!"),
+      ),
+    );
+  }
+}
