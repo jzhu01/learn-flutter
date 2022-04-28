cea07abf4e0a54aa06b08924aaef4e1fb89d1001
Jen Zhu <jennifer0zhu@gmail.com>
2022-04-28 18:16:49 -0400

    adding assets, multiple pages and basic navigation

--- recipe_app/lib/category_meals_screen.dart
+++ recipe_app/lib/category_meals_screen.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
+  final String categoryId;
+  final String categoryTitle;
+
+  CategoryMealsScreen(this.categoryId, this.categoryTitle);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('The Recipes'),
+        title: const Text('categoryTitle'),
       ),
       body: Center(
         child: Text("Recipes for the Category Go here!"),
