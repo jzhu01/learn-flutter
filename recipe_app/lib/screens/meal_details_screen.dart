2a9714d60aec12e3f0cae94da3116a9dafed38e0
Jen Zhu <jennifer0zhu@gmail.com>
2022-05-04 18:32:53 -0400

    Fleshing out the Meal Details screen, refactoring some to make widget tree more readable

--- recipe_app/lib/screens/meal_details_screen.dart
+++ recipe_app/lib/screens/meal_details_screen.dart
@@ -1,23 +1,82 @@
 import 'package:flutter/material.dart';
 
+import '../dummy_data.dart';
+
 class MealDetailsScreen extends StatelessWidget {
   static const routeName = '/meal-details';
 
-  // final String id;
+  Widget buildTitleText(BuildContext context, String text) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Text(text, style: Theme.of(context).textTheme.headline5),
+    );
+  }
 
-  // MealDetailsScreen({@required this.id});
+  Widget buildContainer(Widget childWidget) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      height: 150,
+      width: 300,
+      child: childWidget,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context).settings.arguments as String;
+    final mealData = DUMMY_MEALS.firstWhere((meal) => meal.id == mealId);
     return Scaffold(
       appBar: AppBar(
         title: Text('The Meal: $mealId'),
       ),
-      body: Center(
-          child: Text(
-        'The meal - $mealId!',
-      )),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(mealData.imageUrl, fit: BoxFit.cover),
+            ),
+            buildTitleText(context, "Ingredients"),
+            buildContainer(
+              ListView.builder(
+                itemBuilder: (ctx, index) => Card(
+                  color: Theme.of(context).accentColor,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                    child: Text(mealData.ingredients[index],
+                        style: TextStyle(color: Colors.white)),
+                  ),
+                ),
+                itemCount: mealData.ingredients.length,
+              ),
+            ),
+            buildTitleText(context, "Steps"),
+            buildContainer(
+              ListView.builder(
+                itemBuilder: (context, index) => Column(
+                  children: [
+                    ListTile(
+                      leading: CircleAvatar(
+                        child: Text('# ${(index + 1)}. '),
+                      ),
+                      title: Text(mealData.steps[index]),
+                    ),
+                    Divider(),
+                  ],
+                ),
+                itemCount: mealData.steps.length,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
